bef8ea831e4cf420d1b28f7063e87062ed701964
Fernando Duarte <duarte.brusque@gmail.com>
2023-05-26 22:49:41 -0300

    Uso dos Métodos get e set

--- app/src/main/java/devandroid/fernando/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/fernando/applistacurso/model/Pessoa.java
@@ -1,37 +1,56 @@
 package devandroid.fernando.applistacurso.model;
 
 public class Pessoa {
-    private String nome;
+    private String primeiroNome;
     private String sobreNome;
-    private int telefone;
+    private String cursoDesejado;
+    private String telefoneContato;
 
-    public Pessoa(String nome, String sobreNome, int telefone) {
-        this.nome = nome;
+    public Pessoa(String primeiroNome, String sobreNome, String cursoDesejado, String telefoneContato) {
+        this.primeiroNome = primeiroNome;
         this.sobreNome = sobreNome;
-        this.telefone = telefone;
+        this.cursoDesejado = cursoDesejado;
+        this.telefoneContato = telefoneContato;
+    }
+    public String getPrimeiroNome() {
+        return primeiroNome;
     }
 
-    public String getNome() {
-        return nome;
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
     }
 
     public String getSobreNome() {
         return sobreNome;
     }
 
-    public int getTelefone() {
-        return telefone;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public void setSobreNome(String sobreNome) {
         this.sobreNome = sobreNome;
     }
 
-    public void setTelefone(int telefone) {
-        this.telefone = telefone;
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + getPrimeiroNome() + '\'' +
+                ", sobreNome='" + getSobreNome() + '\'' +
+                ", cursoDesejado='" + getCursoDesejado() + '\'' +
+                ", telefoneContato='" + getTelefoneContato() + '\'' +
+                '}';
     }
 }
--- app/src/main/java/devandroid/fernando/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/fernando/applistacurso/view/MainActivity.java
@@ -9,11 +9,17 @@ import devandroid.fernando.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
-    Pessoa pessoa = new Pessoa("","",0);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Pessoa pessoa = new Pessoa("","","","");
+        pessoa.setPrimeiroNome("Fernando");
+        pessoa.setSobreNome("Duarte");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("47-999553107");
+        pessoa.toString();
+        int parada = 0;
     }
 }
